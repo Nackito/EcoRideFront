6ddbf4619f4a55328007ec707956e50fc1964873
Nackito <nackfrank@gmail.com>
2025-07-13 15:55:36 +0200

    Ajout page offer-seats

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,5 +1,8 @@
 import Route from "./Route.js";
 //Définir ici vos routes
-export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/offer-seats", "Publier un trajet", "/pages/offer-seats.html"),
+];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
