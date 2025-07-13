ba3887ca9eaf29fbf19bf29a14ed04b0e29bae3e
Nackito <nackfrank@gmail.com>
2025-07-13 17:48:28 +0200

    Ajout signin/signup

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,14 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/offer-seats", "Publier un trajet", "/pages/offer-seats.html"),
+  new Route(
+    "/ride-sharing",
+    "Resultats de recherche",
+    "/pages/ride-sharing.html"
+  ),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route("/account", "Mon profil", "/pages/auth/account.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
