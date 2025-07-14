7496c91d03a93999b3a076a8f494d1c4e9cb2ca8
Nackito <nackfrank@gmail.com>
2025-07-14 22:59:57 +0200

    ajout detail-ride

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -11,6 +11,7 @@ export const allRoutes = [
   new Route("/signin", "Connexion", "/pages/auth/signin.html"),
   new Route("/signup", "Inscription", "/pages/auth/signup.html"),
   new Route("/account", "Mon profil", "/pages/auth/account.html"),
+  new Route("/detail-ride", "Détail du trajet", "/pages/detail-ride.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
