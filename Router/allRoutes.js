b83a972ed6feaa7601aa6f0a46c260d8ba5f2af9
Nackito <nackfrank@gmail.com>
2026-02-17 17:18:57 +0100

    fix: Ajouter un utilisateur à la route du profil et corriger la syntaxe des tableaux

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,72 +7,72 @@ export const allRoutes = [
     "Publier un trajet",
     "/pages/offerseats.html",
     [],
-    "/js/ride/ride.js"
+    "/js/ride/ride.js",
   ),
   new Route(
     "/ridesharing",
     "Resultats de recherche",
     "/pages/ridesharing.html",
     [],
-    "/js/ride/resultSearch.js"
+    "/js/ride/resultSearch.js",
   ),
   new Route(
     "/signin",
     "Connexion",
     "/pages/auth/signin.html",
     ["disconnected"],
-    "/js/auth/signin.js"
+    "/js/auth/signin.js",
   ),
   new Route(
     "/signup",
     "Inscription",
     "/pages/auth/signup.html",
     ["disconnected"],
-    "/js/auth/signup.js"
+    "/js/auth/signup.js",
   ),
   new Route(
     "/account",
     "Mon profil",
     "/pages/auth/account.html",
-    ["driver_passenger", "driver", "passenger", "employed", "admin"],
-    "/js/auth/account.js"
+    ["driver_passenger", "driver", "passenger", "employed", "admin", "user"],
+    "/js/auth/account.js",
   ),
   new Route(
     "/detailride",
     "Détail du trajet",
     "/pages/detailride.html",
     [],
-    "/js/ride/detailRide.js"
+    "/js/ride/detailRide.js",
   ),
   new Route(
     "/personalinfo",
     "Mes informations personnelles",
     "/pages/profil/personalinfo.html",
-    ["passenger", "driver", "employed", "driver_passenger" /*, "admin"*/]
+    ["passenger", "driver", "employed", "driver_passenger" /*, "admin"*/],
   ),
   new Route(
     "/carpoolingpreferences",
     "Préférences de covoiturage",
     "/pages/profil/carpoolingpreferences.html",
-    ["driver_passenger", "driver" /*, "admin"*/] // Autorisé aux conducteurs, passagers et employés
+    ["driver_passenger", "driver" /*, "admin"*/], // Autorisé aux conducteurs, passagers et employés
   ),
   new Route(
     "/vehiclemanagement",
     "Gestion des véhicules",
     "/pages/profil/vehiclemanagement.html",
-    ["driver_passenger", "driver" /*, "admin"*/] // Autorisé aux conducteurs, passagers et employés
+    ["driver_passenger", "driver" /*, "admin"*/], // Autorisé aux conducteurs, passagers et employés
   ),
   new Route(
     "/carpoolinghistory",
     "Historique de covoiturage",
     "/pages/profil/carpoolinghistory.html",
-    ["driver_passenger", "driver", "passenger" /*, "admin"*/] // Autorisé aux conducteurs, passagers et employés
+    ["driver_passenger", "driver", "passenger" /*, "admin"*/], // Autorisé aux conducteurs, passagers et employés
   ),
   new Route(
     "/carpoolingmanagement",
     "Gestion de covoiturage",
     "/pages/profil/carpoolingmanagement.html",
-    ["driver_passenger", "driver" /*, "admin"*/] // Autorisé aux conducteurs, passagers et employés
+    ["driver_passenger", "driver" /*, "admin"*/], // Autorisé aux conducteurs, passagers et employés
   ),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
